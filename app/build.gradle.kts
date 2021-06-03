f9231f5eccfdd02f7985e9e4c02b6600f78ce3be
oppongjr <oppong751@gmail.com>
2021-06-03 17:49:14 +0000

    Initial commit.
    Added necessary dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId = "com.mpharma.codetest"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        // enables jetpack compose for this module
+        viewBinding = true
+
+        // Disable unused AGP features
+        buildConfig = false
+        aidl = false
+        renderScript = false
+        resValues = false
+        shaders = false
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    packagingOptions {
+        exclude("META-INF/licenses/**")
+        exclude("META-INF/AL2.0")
+        exclude("META-INF/LGPL2.1")
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val roomVersion = "2.3.0"
+    val hiltVersion = "2.36"
+    val retrofitVersion = "2.9.0"
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
+    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    // add room dependencies
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // add hilt dependencies
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+
+    // retrofit dependencies
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
+    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    testImplementation("androidx.room:room-testing:$roomVersion")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.robolectric:robolectric:4.3")
+
+    testImplementation("androidx.test:runner:1.3.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("androidx.test.ext:junit:1.1.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
+
+kapt {
+    correctErrorTypes = true
+}
