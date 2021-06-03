cb7576cfc28dbe48884755a4d0c4f9b16f0fd6a2
oppongjr <oppong751@gmail.com>
2021-06-03 22:39:40 +0000

    Added empty ProductDetailActivity

---
+++ app/src/main/java/com/mpharma/codetest/ui/activities/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.mpharma.codetest.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mpharma.codetest.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
