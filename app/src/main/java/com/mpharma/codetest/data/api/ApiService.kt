0a249fa25b5d37132282e72fa060104181466e87
oppongjr <oppong751@gmail.com>
2021-06-04 02:13:31 +0000

    Fix: Resolved retrofit error that demanded that base url ended in /

--- app/src/main/java/com/mpharma/codetest/data/api/ApiService.kt
+++ app/src/main/java/com/mpharma/codetest/data/api/ApiService.kt
@@ -3,6 +3,6 @@ package com.mpharma.codetest.data.api
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("/5c3e15e63500006e003e9795")
+    @GET("5c3e15e63500006e003e9795")
     suspend fun getProducts(): ProductResponse
 }
