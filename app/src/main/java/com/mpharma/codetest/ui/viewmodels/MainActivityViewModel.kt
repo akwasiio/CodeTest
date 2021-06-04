6dbf414e02311a512f206a7ea9b779bbe8cd899d
oppongjr <oppong751@gmail.com>
2021-06-04 02:41:22 +0000

    --amend

--- app/src/main/java/com/mpharma/codetest/ui/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/mpharma/codetest/ui/viewmodels/MainActivityViewModel.kt
@@ -1,9 +1,7 @@
 package com.mpharma.codetest.ui.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mpharma.codetest.domain.AppRepository
 import com.mpharma.codetest.domain.AppRepositoryImpl
 import com.mpharma.codetest.ui.ScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,11 +9,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainActivityViewModel @Inject constructor(private val repository: AppRepositoryImpl) : ViewModel() {
+class MainActivityViewModel @Inject constructor(private val repository: AppRepositoryImpl) :
+    ViewModel() {
     private val _state = MutableStateFlow<ScreenState>(ScreenState.Loading)
     val state: StateFlow<ScreenState> = _state
 
@@ -25,9 +25,9 @@ class MainActivityViewModel @Inject constructor(private val repository: AppRepos
 
     private fun getProducts() {
         viewModelScope.launch(Dispatchers.IO) {
-            repository.getProductsWithPrices().collect {
-                Log.e("CollectUpdate", it.size.toString())
-                _state.value = ScreenState.Success(it)
+            repository.getProductsWithPrices().distinctUntilChanged().collect { products ->
+                _state.value =
+                    if (products.isEmpty()) ScreenState.Loading else ScreenState.Success(products)
             }
         }
     }
